b8144d3c2379d5e023ed1faea132efa1f5ecdc1a
Zemian Deng <zemiandeng@gmail.com>
2016-10-22 01:58:27 -0400

    Add zemian-dev/tiny-spring-webapp

---
+++ zemian-dev/tiny-spring-webapp/src/main/java/zemian/spring/tinywebapp/SpringWebappInitializer.java
@@ -0,0 +1,25 @@
+package zemian.spring.tinywebapp;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by zemian on 10/22/16.
+ */
+public class SpringWebappInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { TinyWebappConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/main/*" };
+    }
+
+}
---
+++ zemian-dev/tiny-spring-webapp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<a href="hello">Say Hello</a>
+</body>
+</html>
