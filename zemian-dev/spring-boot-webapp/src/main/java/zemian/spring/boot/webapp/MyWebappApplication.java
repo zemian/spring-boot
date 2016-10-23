7f7ac13e74099ca9d0a9320fa6e25ea6dcb90b0c
Zemian Deng <zemiandeng@gmail.com>
2016-10-23 02:25:58 -0400

    Polish spring-boot-webapp to use SpringBoot based init and config

---
+++ zemian-dev/spring-boot-webapp/src/main/java/zemian/spring/boot/webapp/MyWebappApplication.java
@@ -0,0 +1,11 @@
+package zemian.spring.boot.webapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by zemian on 10/22/16.
+ */
+@SpringBootApplication
+public class MyWebappApplication extends SpringBootServletInitializer {
+}
